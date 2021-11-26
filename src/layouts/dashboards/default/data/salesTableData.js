0e4dee6d7c544cdbffd1fe20ceac4ba68aeeaee6
widjesh <widjesh@satyamholidays.com>
2021-11-25 23:01:00 -0500

    Initial Commit

---
+++ src/layouts/dashboards/default/data/salesTableData.js
@@ -0,0 +1,46 @@
+/**
+=========================================================
+* Soft UI Dashboard PRO React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-pro-material-ui
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Countries flags
+import US from "assets/images/icons/flags/US.png";
+import BR from "assets/images/icons/flags/BR.png";
+
+export default [
+  {
+    country: [US, "Suriname"],
+    Filiaal: "Hoofd Kantoor",
+    Verkoop: "$230,900",
+    bounce: "29.9%",
+  },
+  {
+    country: [US, "Suriname"],
+    Filiaal: "Commewijne",
+    value: "$440,000",
+    bounce: "40.22%",
+  },
+  {
+    country: [US, "Suriname"],
+    Filiaal: "Nickerie",
+    value: "$190,700",
+    bounce: "23.44%",
+  },
+  {
+    country: [US, "Nederland"],
+    Filiaal: "Den Haag",
+    value: "$190,700",
+    bounce: "23.44%",
+  },
+  { country: [BR, "Curacao"], Filiaal: "Willemstad", value: "$143,960", bounce: "32.14%" },
+];
